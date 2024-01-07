5c119418fdc7190c77638983ffe2b0d534b88a1d
Cristian <toccicristian@hotmail.com>
2024-01-07 00:57:51 -0300

    refactor js/iniciajuego.js

--- js/iniciajuego.js
+++ js/iniciajuego.js
@@ -1,5 +1,5 @@
 // jsonFileUrl="../p5l.json"; // habilitar para liveserver
-jsonFileUrl="./p5l.json";  //deshabilitar con liveserver
+jsonFileUrl="./tuttywordle/p5l.json";  //deshabilitar con liveserver
 
  function cargar_palabras(jsonFileUrl){
      return fetch(jsonFileUrl).then((response) => response.json()).then((j) => {
